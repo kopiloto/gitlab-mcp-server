afb69f6685837edacd7ee657ffe4f5bc2882886f
Daniel Pastrana <daniel.pastrana@kavak.com>
2025-06-06 13:41:48 -0600

    Bump version to 0.0.2

--- src/mcp_gitlab_server/__init__.py
+++ src/mcp_gitlab_server/__init__.py
@@ -6,7 +6,7 @@ import gitlab.exceptions
 from mcp.server.fastmcp import FastMCP
 from pydantic import Field
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 server = FastMCP(
     "GitLab MCP Server", dependencies=["python-gitlab"], version=__version__
